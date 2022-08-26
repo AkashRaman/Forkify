5bf9d21a39a0d48891f960fc9e9b1b5156d1d9c9
Akash Raman <akashjayaraman8841@gmail.com>
2022-08-26 20:11:09 +0530

    MCV

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -564,20 +564,6 @@ const showRecipe = async ()=>{
         // 1) Loading recipe
         if (!id) return;
         renderSpinner(recipeContainer);
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-        const data = await res.json();
-        if (!res.ok) throw new Error(`Failed to fetch`);
-        let { recipe  } = data.data;
-        recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            cookingTime: recipe.cooking_time,
-            ingredients: recipe.ingredients
-        };
         console.log(recipe);
         // 2) Rendering recipe
         const markup = `
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,4 @@
+import * as model from './model.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import icons from 'url:../img/icons.svg';
@@ -27,7 +28,7 @@ const renderSpinner = async (parentEL) => {
   `;
   parentEL.innerHTML = '';
   parentEL.insertAdjacentHTML('afterbegin',markup);
-}
+};
 
 const showRecipe = async () => {
   try{
@@ -37,24 +38,10 @@ const showRecipe = async () => {
     if(!id) return;
 
     renderSpinner(recipeContainer);
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await res.json();
-    
-    if(!res.ok) throw new Error(`Failed to fetch`);
 
-    let {recipe} = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients
-    };
-    console.log(recipe);
+    await model.loadRecipe(id);
 
+    model.state.recipe
     // 2) Rendering recipe
     const markup = `
         <figure class="recipe__fig">
