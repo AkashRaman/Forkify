b2df53fc9a933f06194097727008cf4b42832875
Akash Raman <akashjayaraman8841@gmail.com>
2022-08-28 11:32:09 +0530

    MCV Part 3

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-
+import {Fraction} from 'fractional';
+console.log(Fraction);
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
     #data;
@@ -82,20 +83,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-            ${this.#data.ingredients.map(ing => {
-              return `
-              <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="${icons}#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">${ing.unit}</span>
-                  ${ing.description}
-                </div>
-              </li>
-              `
-            }).join('')}
+            ${this.#data.ingredients.map(this.#generateMarkupIng).join('')}
           </ul>
         </div>
 
@@ -103,7 +91,8 @@ class RecipeView {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${this.#data.publisher}</span>. Please check out
+            <span class="reci
+pe__publisher">${this.#data.publisher}</span>. Please check out
             directions at their website.
           </p>
           <a
@@ -119,6 +108,22 @@ class RecipeView {
         </div>
     `;
     }
+
+  #generateMarkupIng(ing) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
+    `
+    
+  }
 }
 
 export default new RecipeView();
